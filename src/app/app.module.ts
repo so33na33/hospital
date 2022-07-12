03eeec86118089628ab07998807c35f849ff95b1
so33na33 <santhoshsona150@gmail.com>
2022-07-12 22:42:34 +0530

    updated search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HospitalComponent } from './hospital/hospital.component';
 import { SearchComponent } from './search/search.component';
+import { ViewDoctorsComponent } from './view-doctors/view-doctors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HospitalComponent,
-    SearchComponent
+    SearchComponent,
+    ViewDoctorsComponent
   ],
   imports: [
     BrowserModule,
