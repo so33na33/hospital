38e010f23a7d71db725bc43f7abcb5b348eb43e8
so33na33 <santhoshsona150@gmail.com>
2022-07-12 20:45:46 +0530

    front page

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HospitalComponent } from './hospital/hospital.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HospitalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
